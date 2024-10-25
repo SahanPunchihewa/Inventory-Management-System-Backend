153372d4ffeeb7ac1dc3691a7629008761f8e16d
Sahan Punchihewa <sahannilupul18@gmail.com>
2024-10-25 23:25:05 +0530

    Complete product backend APIs

--- Services/ProductService.cs
+++ Services/ProductService.cs
@@ -17,8 +17,32 @@ namespace InventoryManagementSystemAPI.Services
         public Product Create(Product product)
         {
             _product.InsertOne(product);
-            return product;
-        
+            return product;        
+        }
+
+        // Get all product details
+        public List<Product> Get()
+        {
+            return _product.Find(product => true).ToList();
+        }
+
+        // Get product by ID
+        public Product GetById(string id)
+        {
+            return _product.Find(product => product.Id == id).FirstOrDefault();
+        }
+
+        // Delete product
+        public void DeleteProduct(string id)
+        {
+          _product.DeleteOne(product => product.Id == id);  
+        }
+
+        // Update Product
+        public void UpdateProduct(string id, Product product)
+        {
+
+           _product.ReplaceOne(product => product.Id == id, product);
         }
     }
 }
